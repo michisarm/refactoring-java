96f5e4a3da9c54faf643c682b0cfc15eaac41a7b
이준우 <michisarm@gmail.com>
2022-03-02 13:48:16 +0900

    feat : 메서드 호출로 전환

--- chapter01-맛보기예제/src/main/java/Customer.java
+++ chapter01-맛보기예제/src/main/java/Customer.java
@@ -21,11 +21,7 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = get_name() + " 고객님의 대여기록\n";
         while (rentals.hasMoreElements()){
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            //비디오 종류별 대여료 계산
-            thisAmount = each.getCharge();
 
             // 적립 포인트를 1포인트 증가
             frequentRenterPoints ++;
@@ -34,10 +30,10 @@ public class Customer {
                 frequentRenterPoints ++;
 
             // 이번에 대여하는 비디오 정보와 대여료를 출력
-            result += "\t" + each.get_movie().get_title() + "\t" + thisAmount + "\n";
+            result += "\t" + each.get_movie().get_title() + "\t" + each.getCharge() + "\n";
 
             // 현재까지 누적된 총 대여료
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
         }
         // 푸터 행 추가
         result += "누적 대여료: " + totalAmount + "\n";
