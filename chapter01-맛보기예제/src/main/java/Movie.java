d1acaba4fc0d98842f5771740c18d65da1dbdaa9
brandon <brandon@ijun-uui-Macmini.local>
2022-03-04 13:25:43 +0900

    feat : getCharge 메서드 Rental-> Movie 이동

--- chapter01-맛보기예제/src/main/java/Movie.java
+++ chapter01-맛보기예제/src/main/java/Movie.java
@@ -16,4 +16,33 @@ public class Movie {
         this._title = _title;
         this._priceCode = _priceCode;
     }
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (get_priceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if(daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if(daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        // 최신물을 이틀 이상 대여하면 2포인트 지급하고 그 외엔 1포인트 지급하는 코드를
+        // 빼내 getFrequentRenterPoints 메서드로 만들고 이 Rental 클래스로 옮겼다.
+        if((get_priceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
--- chapter01-맛보기예제/src/main/java/Rental.java
+++ chapter01-맛보기예제/src/main/java/Rental.java
@@ -11,31 +11,10 @@ public class Rental {
     }
 
     double getCharge() {
-        double result = 0;
-        switch (get_movie().get_priceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if(get_daysRented() > 2)
-                    result += (get_daysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += get_daysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if(get_daysRented() > 3)
-                    result += (get_daysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     int getFrequentRenterPoints() {
-        // 최신물을 이틀 이상 대여하면 2포인트 지급하고 그 외엔 1포인트 지급하는 코드를
-        // 빼내 getFrequentRenterPoints 메서드로 만들고 이 Rental 클래스로 옮겼다.
-        if((get_movie().get_priceCode() == Movie.NEW_RELEASE) && get_daysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
