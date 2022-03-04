c396cae15d836b22453aaaf5eb3daa1accc1e3e5
brandon <brandon@ijun-uui-Macmini.local>
2022-03-04 17:07:52 +0900

    feat : 상태패턴 적용1 단계 - 분류 부호를 상태/전략 패턴으로 전환

--- chapter01-맛보기예제/src/main/java/Movie.java
+++ chapter01-맛보기예제/src/main/java/Movie.java
@@ -9,12 +9,31 @@ public class Movie {
     @Getter
     private String _title;
 
-    @Getter @Setter
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String _title, int _priceCode) {
         this._title = _title;
-        this._priceCode = _priceCode;
+        set_priceCode(_priceCode);
+    }
+
+    public int get_priceCode() {
+        return _price.getPriceCode();
+    }
+
+    private void set_priceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("가격 코드가 잘못됐습니다.");
+        }
     }
 
     double getCharge(int daysRented) {
