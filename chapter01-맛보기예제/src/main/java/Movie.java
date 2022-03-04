4b55f305698e660d19f7cac020a3251f7d4f9049
brandon <brandon@ijun-uui-Macmini.local>
2022-03-04 18:34:10 +0900

    feat : getFrequentRenterPoints 상태패턴 적용 - 이때 method 추상화가 아닌 override 로 진행

--- chapter01-맛보기예제/src/main/java/Movie.java
+++ chapter01-맛보기예제/src/main/java/Movie.java
@@ -1,5 +1,4 @@
 import lombok.Getter;
-import lombok.Setter;
 
 public class Movie {
     public static final int CHILDRENS = 2;
@@ -41,11 +40,6 @@ public class Movie {
     }
 
     int getFrequentRenterPoints(int daysRented) {
-        // 최신물을 이틀 이상 대여하면 2포인트 지급하고 그 외엔 1포인트 지급하는 코드를
-        // 빼내 getFrequentRenterPoints 메서드로 만들고 이 Rental 클래스로 옮겼다.
-        if((get_priceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        else
-            return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
